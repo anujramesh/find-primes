323ed3dffa0944cb329e12877a40875a4fb93122
anujramesh <37349553+anujramesh@users.noreply.github.com>
2021-01-12 12:29:13 -0500

    Update FindPrimes.java

--- FindPrimes.java
+++ FindPrimes.java
@@ -1,18 +1,17 @@
 import java.util.LinkedList;
 
 public class FindPrimes {
-
-
-
+    
     public static void main(String[] args) {
 
         LinkedList<Integer> listOfPrimes = new LinkedList<Integer>();
+        final int RANGE = 1000000;
         int upperBound;  // The largest possible prime factor for an integer i
         int count = 1;
         System.out.println(count + " " + 2);    // Prints 2, the first prime
         count++;
 
-        for (int i = 3; i <= 10000000; i = i + 2) {
+        for (int i = 3; i <= RANGE; i = i + 2) {
             upperBound = (int) Math.sqrt(i); // The largest prime factor is equal to the floor of the square root of i
             boolean isPrime = true;
 
